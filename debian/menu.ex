fd374d34596fe43cb2967a08ce05d22a81a894d1
Johan Wermensjö <johanwermensjoe@gmail.com>
2016-02-05 22:09:43 +0100

    Cleaned up in debian files.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pilight-cc):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pilight-cc" command="/usr/bin/pilight-cc"
--- debian/pilight-cc.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pilight-cc package
-#
-0 4	* * *	root	[ -x /usr/bin/pilight-cc_maintenance ] && /usr/bin/pilight-cc_maintenance
