7f9d2dcf2069d0706a34481a9d1e6efe54685907
Johan Wermensjö <johanwermensjoe@gmail.com>
2016-03-21 21:36:38 +0100

    Added base protobuf impl. + gitignore.

---
+++ pilightcc.py
@@ -0,0 +1,21 @@
+from gi.repository import Gtk
+import message_pb2
+
+def hello(button):
+    print("Hello World!\n")
+    msg = message_pb2.ColorRequest()
+    msg.priority = 50
+    msg.RgbColor = 0x00FF0000
+    msg.duration = 1000
+    print(": " + msg.SerializeToString())
+
+builder = Gtk.Builder()
+builder.add_from_file("pilightcc.glade")
+handlers = {
+    "onDeleteWindow": Gtk.main_quit,
+    "onButtonPressed": hello
+}
+builder.connect_signals(handlers)
+window = builder.get_object("window1")
+window.show_all()
+Gtk.main()
