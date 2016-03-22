ae4532ab675c039cf4e021ad107deb5f1143b850
Johan Wermensjö <johanwermensjoe@gmail.com>
2016-03-23 00:24:42 +0100

    Improved tests and added conditional hyperion tests.

--- test/test_capture.py
+++ test/test_capture.py
@@ -8,29 +8,32 @@ class CaptureTestCase(unittest.TestCase):
     def test_capture_format(self):
         pb = capture.get_pixel_buffer()
         print "\nCapture format:"
-        print "Channels: " + str(pb.get_n_channels())
+        print "Size: {0}x{1}".format(pb.get_width(), pb.get_height())
+        print "Channels: {0}".format(str(pb.get_n_channels()))
         self.assertEqual(pb.get_n_channels(), 3)
-        print "Byte length: " + str(pb.get_byte_length())
+        print "Byte length: {0}".format(str(pb.get_byte_length()))
         self.assertEqual(pb.get_byte_length(),
                          pb.get_n_channels() * pb.get_height() * pb.get_width())
-        print "Sample bits: " + str(pb.get_bits_per_sample())
+        print "Sample bits: {0}".format(str(pb.get_bits_per_sample()))
         self.assertEqual(pb.get_bits_per_sample(), 8)
 
     def test_capture_scaling(self):
+        scale = 2
         pb = capture.get_pixel_buffer()
-        pb2 = capture.scale_pixel_buffer(pb, pb.get_width() / 2,
-                                         pb.get_height() / 2)
-        self.assertEqual(pb.get_byte_length(), pb2.get_byte_length() * 4)
+        pb2 = capture.scale_pixel_buffer(pb, pb.get_width() / scale,
+                                         pb.get_height() / scale)
+        self.assertEqual(pb.get_byte_length(),
+                         pb2.get_byte_length() * scale**2)
 
     def test_capture_rate(self):
-        def test():
+        def capture_and_scale():
             pb = capture.get_pixel_buffer()
             capture.scale_pixel_buffer(pb, pb.get_width() / 2,
                                        pb.get_height() / 2)
 
-        fps = 100 / timeit(test, number=100)
+        fps = 100 / timeit(capture_and_scale, number=100)
         print "\nCapture rate:"
-        print "Avg fps: " + str(fps)
+        print "Avg fps: {0}".format(str(fps))
         self.assertGreaterEqual(fps, 30)
 
 
